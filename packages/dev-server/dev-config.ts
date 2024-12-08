054d33d8015ca791d5667f3008f675a7ea57a93d
phuong.le <phuong.le@neopay.vn>
2024-12-08 08:00:31 +0700

    update config

--- packages/dev-server/dev-config.ts
+++ packages/dev-server/dev-config.ts
@@ -162,11 +162,11 @@ function getDbConfig(): DataSourceOptions {
             return {
                 synchronize: true,
                 type: 'mariadb',
-                host: '127.0.0.1',
-                port: 3306,
-                username: 'vendure',
-                password: 'password',
-                database: 'vendure-dev',
+                host: process.env.DB_HOST || 'localhost',
+                port: Number(process.env.DB_PORT) || 5432,
+                username: process.env.DB_USERNAME || 'vendure',
+                password: process.env.DB_PASSWORD || 'password',
+                database: process.env.DB_NAME || 'vendure-dev',
             };
     }
 }
